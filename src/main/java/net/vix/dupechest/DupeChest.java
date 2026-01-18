c36150065709e3b697e57831e02e386be4532ae3
Vix. <vixnf05@gmail.com>
2026-01-18 13:11:26 +0800

    first commit fail, trying again!

---
+++ src/main/java/net/vix/dupechest/DupeChest.java
@@ -0,0 +1,16 @@
+package net.vix.dupechest;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DupeChest implements ModInitializer {
+	public static final String MOD_ID = "dupechest";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/vix/dupechest/DupeChestClient.java
@@ -0,0 +1,10 @@
+package net.vix.dupechest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DupeChestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
